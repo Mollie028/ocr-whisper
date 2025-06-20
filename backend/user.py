0e4e8794cd1b92b4f739dd4e94686cbefdb1db3c
Mollie028 <y840530@gmail.com>
2025-06-20 17:55:47 +0800

    Update and rename backend/schemas/user.py to backend/user.py

---
+++ backend/user.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+    email: str
+    role: str
+
+class UserInDB(UserOut):
+    hashed_password: str
+
+
