bd75bf2d52845455c7d7938d6d563a5b5f739d5e
Mollie028 <y840530@gmail.com>
2025-06-24 16:03:13 +0800

    Update db.py

--- backend/core/db.py
+++ backend/core/db.py
@@ -1,9 +1,22 @@
-import os
 import psycopg2
+import os
 from psycopg2.extras import RealDictCursor
+import urllib.parse as up
 
 def get_conn():
+    dsn = os.getenv("DATABASE_URL")
+    if not dsn:
+        raise Exception("❌ DATABASE_URL 未設定")
+
+    up.uses_netloc.append("postgres")
+    url = up.urlparse(dsn)
+
     return psycopg2.connect(
-        os.getenv("DATABASE_URL"),
-        cursor_factory=RealDictCursor
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port,
+        cursor_factory=RealDictCursor,
+        sslmode="require"
     )
