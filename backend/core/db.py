4e3a7910e04cb0564b4eaab28710883fd6e6ad1a
Mollie028 <y840530@gmail.com>
2025-06-24 14:46:50 +0800

    Update db.py

--- backend/core/db.py
+++ backend/core/db.py
@@ -1,13 +1,9 @@
+import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
-import os
 
 def get_conn():
     return psycopg2.connect(
-        host=os.getenv("POSTGRES_HOST", "localhost"),
-        port=os.getenv("POSTGRES_PORT", 5432),
-        user=os.getenv("POSTGRES_USER", "postgres"),
-        password=os.getenv("POSTGRES_PASSWORD", "password"),
-        dbname=os.getenv("POSTGRES_DB", "postgres"),
+        os.getenv("DATABASE_URL"),
         cursor_factory=RealDictCursor
     )
