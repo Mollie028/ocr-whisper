8cc1558139044910f3fe680c779d24732fc7693f
Mollie028 <y840530@gmail.com>
2025-06-26 16:39:10 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 echo "🚀 Running entrypoint.sh"
 
 export PYTHONPATH=/app  # ✅ 這行一定要有
-exec uvicorn backend.main:app --host=0.0.0.0 --port=8000 --reload
+exec uvicorn backend.main:app --host=0.0.0.0 --port=8000
